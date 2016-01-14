e5fc4b9f4ca2dd7dcff5de52b4b554c0b4650524
Mathieu Jacques <matjacques@hotmail.com>
2016-01-14 16:02:17 -0500

    Fixes the rgs file and use it when generating the manifest to make sure the threadingModel attribute is added.

--- LightjamsSpout/LightjamsSpout/LightjamsSpout_i.c
+++ LightjamsSpout/LightjamsSpout/LightjamsSpout_i.c
@@ -6,7 +6,7 @@
 
 
  /* File created by MIDL compiler version 8.00.0603 */
-/* at Sun Nov 15 14:50:35 2015
+/* at Thu Jan 14 15:58:59 2016
  */
 /* Compiler settings for LightjamsSpout.idl:
     Oicf, W1, Zp8, env=Win32 (32b run), target_arch=X86 8.00.0603 
